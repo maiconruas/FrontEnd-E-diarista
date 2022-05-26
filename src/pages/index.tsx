6f57bc66ab7192ea1a5a0af7a8439329c9c4d8b2
Maicon <maicon_ruas@hotmail.com>
2022-05-26 15:48:00 -0300

    Finalizando a segunda parte do projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,22 @@ import {NextPage} from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import {Button, CircularProgress, Container, Typography} from "@mui/material";
+import {FormElementsContainer, ProfissionalContainer, ProfissionalPapper} from "@styles/pages/index.style";
+import useIndex from "data/hooks/pages/useIndex.page";
 
 const Home: NextPage = () => {
+    const { cep,
+        setCep,
+        cepValido,
+        buscarProfissionais,
+        erro,
+        diaristas,
+        buscaFeita,
+        carregando,
+        diaristasRestantes} = useIndex();
+
   return (
     <div>
       <SafeEnvironment />
@@ -12,14 +26,64 @@ const Home: NextPage = () => {
             subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
         />
 
-      <UserInformation
-        name={'Maicon Ruas'}
-        picture={'http://github.com/maiconruas.png'}
-        rating={4}
-        description={'Sapucaia do Sul'}
-      />
+        <Container>
+            <FormElementsContainer>
+                <TextFieldMask mask={'99.999-999'} label={'Digite seu CEP'} fullWidth variant={'outlined'}
+                               value={cep} onChange={(event) => setCep(event.target.value)}/>
+
+                {erro && <Typography color={'error'}>{erro}</Typography>}
+
+                <Button variant={'contained'} color={'secondary'} sx={{ width: '220px'}} disabled={!cepValido || carregando} onClick={() => buscarProfissionais(cep)}>
+                    {carregando ? <CircularProgress size={20}/> : 'Buscar'}
+                </Button>
+            </FormElementsContainer>
+
+            {buscaFeita &&
+            (diaristas.length > 0 ? (
+                <ProfissionalPapper>
+                    <ProfissionalContainer>
+                        {diaristas.map((item, index) => {
+                            return (
+                                <UserInformation
+                                    key={index}
+                                    name={item.nome_completo}
+                                    picture={item.foto_usuario}
+                                    rating={item.reputacao}
+                                    description={item.cidade}
+                                />
+                            );
+                        })}
+                    </ProfissionalContainer>
+
+                    <Container sx={{ textAlign: 'center' }}>
+                        {diaristasRestantes > 0 && (
+                            <Typography sx={{ mt: 5 }}>
+                                ...e mais {diaristasRestantes}{' '}
+                                {diaristasRestantes > 1
+                                    ? 'profissionais atendem'
+                                    : 'profissional atende'}{' '}
+                                ao seu endereço.
+                            </Typography>
+                        )}
+
+                        <Button
+                            variant={'contained'}
+                            color={'secondary'}
+                            sx={{ mt: 5 }}
+                        >
+                            Contratar um profissional
+                        </Button>
+                    </Container>
+                </ProfissionalPapper>
+            ) : (
+                <Typography align={'center'} color={'textPrimary'}>
+                    Ainda não temos nenhuma diarista disponível em sua
+                    região
+                </Typography>
+            ))}
+        </Container>
     </div>
-  )
+  );
 }
 
 export default Home
